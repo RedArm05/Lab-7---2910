d3f303c77d13513853e8d1082d37d722f39d9461
RedArm <160531209+RedArm05@users.noreply.github.com>
2025-04-27 19:11:10 -0400

    Update Program.cs

--- Lab5 - Elijah Mckeehan/Program.cs
+++ Lab5 - Elijah Mckeehan/Program.cs
@@ -21,7 +21,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles(); 
 app.UseAntiforgery(); 
 
-app.MapRazorComponents<App>();
-    .AddInteractiveServerRenderMode();
+builder.Services.AddRazorComponents()
+    .AddInteractiveComponents<App>();
 
 app.Run(); 
