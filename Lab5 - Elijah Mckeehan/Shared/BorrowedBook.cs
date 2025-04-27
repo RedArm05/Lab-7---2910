aa6d85df9845efabc4604043fb20e0c0f0e483c8
RedArm <160531209+RedArm05@users.noreply.github.com>
2025-04-27 16:28:21 -0400

    Update BorrowedBook.cs

--- Lab5 - Elijah Mckeehan/Shared/BorrowedBook.cs
+++ Lab5 - Elijah Mckeehan/Shared/BorrowedBook.cs
@@ -1,18 +1,20 @@
-﻿namespace Lab5_Elijah_Mckeehan.Shared
+namespace Lab5_Elijah_Mckeehan.Shared
 {
     public class BorrowedBook
     {
         public int UserId { get; set; }
         public int BookId { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
         public DateTime BorrowedDate { get; set; }
         public DateTime? ReturnDate { get; set; }
 
-        public BorrowedBook(int userId, int bookId)
+        public BorrowedBook(int userId, int bookId, string title, string author)
         {
             UserId = userId;
             BookId = bookId;
+            Title = title;
+            Author = author;
             BorrowedDate = DateTime.Now;
         }
     }
